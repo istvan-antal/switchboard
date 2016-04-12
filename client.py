0fd019fec6bef4a220e651dcb2fbfa7314d5bce3
István Antal <istvan.m.antal@gmail.com>
2016-04-12 12:02:44 +0100

    Endpoint is now a parameter

--- client.py
+++ client.py
@@ -4,11 +4,17 @@ import time
 import hmac
 import hashlib
 import json
+import argparse
 
 with open("client.json") as data_file:
     config = json.load(data_file)
 
-path_and_query = "/switch/0/off?TIMESTAMP=" + str(int(time.time())) + "&ACCOUNT_ID=" + config['id']
+parser = argparse.ArgumentParser()
+parser.add_argument("endpoint", help="API endpoint to call",
+                    type=str)
+args = parser.parse_args()
+
+path_and_query = "/" + args.endpoint + "?TIMESTAMP=" + str(int(time.time())) + "&ACCOUNT_ID=" + config['id']
 host = config['host']
 sig=hmac.new(str(config['secret']), msg=path_and_query, digestmod=hashlib.sha1).hexdigest()
 req = requests.get(host+path_and_query, headers={'X-Auth-Signature': sig})
