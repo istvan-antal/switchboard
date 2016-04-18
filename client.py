e0f2fa0faa4ff56f23f8ebc740f49b387ccc10aa
István Antal <istvan.m.antal@gmail.com>
2016-04-18 12:32:05 +0100

    Started turning client into a reusable module

--- client.py
+++ client.py
@@ -6,16 +6,19 @@ import hashlib
 import json
 import argparse
 
-with open("client.json") as data_file:
-    config = json.load(data_file)
+def get(path):
+    path_and_query = "/" + path + "?TIMESTAMP=" + str(int(time.time())) + "&ACCOUNT_ID=" + config['id']
+    host = config['host']
+    sig=hmac.new(str(config['secret']), msg=path_and_query, digestmod=hashlib.sha1).hexdigest()
+    return requests.get(host+path_and_query, headers={'X-Auth-Signature': sig})
 
-parser = argparse.ArgumentParser()
-parser.add_argument("endpoint", help="API endpoint to call",
-                    type=str)
-args = parser.parse_args()
+if __name__ == "__main__":
+    with open("client.json") as data_file:
+        config = json.load(data_file)
 
-path_and_query = "/" + args.endpoint + "?TIMESTAMP=" + str(int(time.time())) + "&ACCOUNT_ID=" + config['id']
-host = config['host']
-sig=hmac.new(str(config['secret']), msg=path_and_query, digestmod=hashlib.sha1).hexdigest()
-req = requests.get(host+path_and_query, headers={'X-Auth-Signature': sig})
-print req
+    parser = argparse.ArgumentParser()
+    parser.add_argument("endpoint", help="API endpoint to call",
+                        type=str)
+    args = parser.parse_args()
+    
+    print get(args.endpoint)
