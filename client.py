0a4bf1edb3ef99d35370cd9448ad9262df4a9c16
István Antal <istvan.m.antal@gmail.com>
2016-04-18 13:57:10 +0100

    client.py now provides a class

--- client.py
+++ client.py
@@ -6,19 +6,23 @@ import hashlib
 import json
 import argparse
 
-def get(path):
-    path_and_query = "/" + path + "?TIMESTAMP=" + str(int(time.time())) + "&ACCOUNT_ID=" + config['id']
-    host = config['host']
-    sig=hmac.new(str(config['secret']), msg=path_and_query, digestmod=hashlib.sha1).hexdigest()
-    return requests.get(host+path_and_query, headers={'X-Auth-Signature': sig})
+class Client(object):
+    def __init__(self, config):
+        self._config = config
+
+    def get(self, path):
+        path_and_query = "/" + path + "?TIMESTAMP=" + str(int(time.time())) + "&ACCOUNT_ID=" + self._config['id']
+        host = self._config['host']
+        sig=hmac.new(str(self._config['secret']), msg=path_and_query, digestmod=hashlib.sha1).hexdigest()
+        return requests.get(host+path_and_query, headers={'X-Auth-Signature': sig})
 
 if __name__ == "__main__":
     with open("client.json") as data_file:
-        config = json.load(data_file)
+        client = Client(json.load(data_file))
 
     parser = argparse.ArgumentParser()
     parser.add_argument("endpoint", help="API endpoint to call",
                         type=str)
     args = parser.parse_args()
-    
-    print get(args.endpoint)
+
+    print client.get(args.endpoint)
