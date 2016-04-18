ff6c4d0460ad032a936a574dfbdefe341dc34993
István Antal <istvan.m.antal@gmail.com>
2016-04-18 19:47:59 +0100

    Fixed lamp logic

--- desklamp.py
+++ desklamp.py
@@ -16,35 +16,25 @@ class DeskLamp(object):
         DeskLamp.lamp_count += 1
         s = sun(lat=lat, long=long)
 
-        def job():
+        def job(isInitial=False):
             current_time = datetime.now().time()
             wake_up_time = datetime.now().time()
             wake_up_time = wake_up_time.replace(6, 30, 0)
 
-            if current_time < wake_up_time:
-                print "We are before wakeup time, lamp off"
+            if s.sunrise() < current_time and current_time < s.sunset():
+                print "Sun is up, lamp should be off"
                 board.turn_off(switchIndex)
                 return
 
-            if current_time < s.sunrise() or current_time > s.sunset():
+            if current_time < wake_up_time:
                 print "Time to swich on the lamp"
-                board.turn_on(switchIndex)
+                board.turn_off(switchIndex)
                 return
 
-            noon_time = s.solarnoon()
-            if current_time > noon_time:
-                print "Let's wait till sunset before we switch on the lamp"
-                delta = datetime.combine(date.today(), s.sunset()) - datetime.combine(date.today(), current_time)
-                time.sleep(delta.total_seconds() + 1)
-                return job()
+            print "Lamp time"
+            board.turn_on(switchIndex)
 
-            print "Sun is up, lamp should be off"
-            board.turn_off(switchIndex)
-
-        schedule.every().day.at("6:30").do(job)
-        schedule.every().day.at("9:00").do(job)
-        schedule.every().day.at("19:00").do(job)
-        schedule.every().day.at("00:01").do(job)
+        schedule.every().hour.do(job)
         job()
 
         if DeskLamp.t is None:
