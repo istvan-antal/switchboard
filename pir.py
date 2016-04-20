e7ef71c7ba3a53705ca0accc992fad3e2f75de77
István Antal <istvan.m.antal@gmail.com>
2016-04-20 23:04:01 +0100

    Read pinNumber form config key

--- pir.py
+++ pir.py
@@ -3,17 +3,17 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 
 class Sensor:
-    def __init__(self, pinNumber):
-        GPIO.setup(pinNumber, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    def __init__(self, config):
+        GPIO.setup(config["pinNumber"], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
         self.on_motion = None
 
         # Callback function to run when motion detected
         def motionSensor(channel):
-            if GPIO.input(pinNumber):     # True = Rising
+            if GPIO.input(config["pinNumber"]):     # True = Rising
                 print "Motion detected"
                 if self.on_motion is not None:
                     self.on_motion()
 
         # add event listener on pin 21
-        GPIO.add_event_detect(pinNumber, GPIO.BOTH, callback=motionSensor, bouncetime=300)
+        GPIO.add_event_detect(config["pinNumber"], GPIO.BOTH, callback=motionSensor, bouncetime=300)
